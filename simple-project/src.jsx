200e8bf3d949a678d89f77b90dc445c140c05ceb
Madhusudan <madhusudan07.code@gmail.com>
2026-09-21 13:10:32 +0530

    Update src.jsx

--- simple-project/src.jsx
+++ simple-project/src.jsx
@@ -2,5 +2,18 @@ import React from "react";
 import {createRoot} from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
 import "./style.css";
-function App(){return <div className="page"><h1>Hello Guys👋</h1><p>My first Vercel website.</p><button>Click Me</button></div>}
-createRoot(document.getElementById("root")).render(<App/>);
+function App() {
+  return (
+    <>
+      <div className="page">
+        <h1>Hello 👋</h1>
+        <p>My first Vercel website.</p>
+        <button>Click Me</button>
+      </div>
+
+      <Analytics />
+    </>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<App />);
