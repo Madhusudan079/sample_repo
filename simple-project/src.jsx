ce62fcfffd3b1a6841eff0cc606601181cc69368
Madhusudan <madhusudan07.code@gmail.com>
2026-09-21 12:42:03 +0530

    Add files via upload

---
+++ simple-project/src.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import {createRoot} from "react-dom/client";
+import "./style.css";
+function App(){return <div className="page"><h1>Hello 👋</h1><p>My first Vercel website.</p><button>Click Me</button></div>}
+createRoot(document.getElementById("root")).render(<App/>);
